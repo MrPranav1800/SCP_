6f65dd5ab95ebdd90ab9862ceba4d117b4c93f45
MrPranav1800 <120796213+MrPranav1800@users.noreply.github.com>
2023-01-09 15:03:40 +0530

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -1,6 +1,9 @@
 A =int(input("enter a number: "))
-if A
+if A not an int:
+  print("type numbers only")
 B = int(input("enter another number: "))
+if B is not an int:
+  print("type numbers only")
 C=A+B
 
 print (C)
